bbaa3454d86d1961fc977d298460a9f8a5672b93
p4ntoufle <lou.mai52152@gmail.com>
2026-04-10 15:28:44 +0200

    Fix: Use correct CSRF middleware namespace for Laravel 13

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
         
