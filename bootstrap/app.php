368c7f3642c5073c88c1fe2d626fd68c9e48980c
p4ntoufle <lou.mai52152@gmail.com>
2026-04-10 14:05:29 +0200

    FIX adding middleware headers2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,18 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Trust proxies for reverse proxy scenarios
-        $proxies = env('TRUSTED_PROXIES', '127.0.0.1');
-        $headers = env('TRUSTED_HEADERS', 
-            \Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_FOR |
-            \Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_HOST |
-            \Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_PROTO
-        );
-        
-        $middleware->trustProxies(
-            at: explode(',', $proxies),
-            headers: $headers
-        );
+        // Trust all proxies for reverse proxy scenarios
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
